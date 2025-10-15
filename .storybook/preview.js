a080254138201f08c78115f376bec3bcb7fc18d2
Abraham Duno <abraham@spidipagos.com>
2025-10-14 23:23:19 -0400

    structure init

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,37 @@
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
+    options: {
+      storySort: {
+        order: [
+          'Introduction',
+          'API', [
+            'Acuerdo',
+            'Sesion Boton de Pago', [
+              '0. Resumen',
+              '1. Crear Sesion B',
+              // '2. Obtener Sesion B',
+              // '3. Actualizar Sesion B',
+              // '4. Eliminar Sesion B',
+            ],
+            'Parada',
+            'Sesion Solicitud de Pago', [
+              '0. Resumen',
+              '1. Crear Sesion S',
+              // '2. Obtener Sesion S',
+              // '3. Actualizar Sesion S',
+              // '4. Eliminar Sesion S',
+            ]
+          ],
+          'Examples', [
+            'Servicios', [
+              'Autenticacion',
+            ],
+            'Playbook',
+          ]
+        ],
+      },
+    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
